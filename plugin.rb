2b761a2ffe8829bbe2d945209e1f83d8479f120a
Brandon Cryderman <GenericCPU@users.noreply.github.com>
2020-07-09 22:52:14 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,26 +3,21 @@
 # version: 0.1.0
 # author: parasquid
 
-gem 'omniauth-mindvalley'
+gem 'handcash-connect-auth'
 
 
-class MindvalleyAuthenticator < ::Auth::Authenticator
-
-  CLIENT_ID = '1111111'
-  CLIENT_SECRET = 'AUSTRALIASNAKESALAD'
+class HandCashConnectAuthenticator < ::Auth::Authenticator
 
   def name
-    'mindvalley'
+    'handcash-connect'
   end
 
-  def after_authenticate(auth_token)
+  def after_authenticate(data)
     result = Auth::Result.new
 
     # grap the info we need from omni auth
-    data = auth_token[:info]
-    name = data["first_name"]
-    mv_uid = auth_token["uid"]
-    email = data['email']
+    authToken = data['authToken']
+    signingToken = data['signingToken']
 
     # plugin specific data storage
     current_info = ::PluginStore.get("mv", "mv_uid_#{mv_uid}")
@@ -32,9 +27,8 @@ class MindvalleyAuthenticator < ::Auth::Authenticator
         User.where(id: current_info[:user_id]).first
       end
 
-    result.name = name
-    result.extra_data = { mv_uid: mv_uid }
-    result.email = email
+    result.authToken = authToken
+    result.signingToken = signingToken
 
     result
   end
@@ -45,15 +39,13 @@ class MindvalleyAuthenticator < ::Auth::Authenticator
   end
 
   def register_middleware(omniauth)
-    omniauth.provider :mindvalley,
-     CLIENT_ID,
-     CLIENT_SECRET
+    handcash.provider :handcash,
   end
 end
 
 
-auth_provider :title => 'with Mindvalley Accounts',
-    :message => 'Log in via Mindvalley Accounts (Make sure pop up blockers are not enabled).',
+auth_provider :title => 'with HandCash Accounts',
+    :message => 'Log in via HandCash',
     :frame_width => 920,
     :frame_height => 800,
     :authenticator => MindvalleyAuthenticator.new
